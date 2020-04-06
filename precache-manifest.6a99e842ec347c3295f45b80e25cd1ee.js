1d44824fad117b958491c5c330df936fe069ec36
Alin402 <balanescu.alin03@gmail.com>
2020-04-07 02:26:34 +0300

    Updates

---
+++ precache-manifest.6a99e842ec347c3295f45b80e25cd1ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4a028c098a3fa7d92a9821d71d921c9",
+    "url": "/Devconnector/index.html"
+  },
+  {
+    "revision": "a792eb263f4a5f0c9680",
+    "url": "/Devconnector/static/css/main.9d5415ee.chunk.css"
+  },
+  {
+    "revision": "bad3629ad32f0047a653",
+    "url": "/Devconnector/static/js/2.e974fa89.chunk.js"
+  },
+  {
+    "revision": "f9e9f10d9da53b47a9063c891ed2eede",
+    "url": "/Devconnector/static/js/2.e974fa89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a792eb263f4a5f0c9680",
+    "url": "/Devconnector/static/js/main.8aec91d3.chunk.js"
+  },
+  {
+    "revision": "a8de4796dcda88d749dc",
+    "url": "/Devconnector/static/js/runtime-main.c44b4a34.js"
+  },
+  {
+    "revision": "584b607f5c2ff075429dc0e7b8d142ef",
+    "url": "/Devconnector/static/media/loading.584b607f.gif"
+  }
+]);
